abacbe0e37dbd30c264632d8b3650d1512ea5e71
Monish Das <monishdas2003@gmail.com>
2025-12-19 20:34:42 -0500

    feat: Blockchain project added to repo

---
+++ Block.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Block {
+    public String hash; // holds digital signature
+    public String previousHash; // holds previous block's hash and data
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+    private long timeStamp; // displayed in number of milliseconds
+    private int nonce;
+
+    // Block Constructor.
+    public Block(String previousHash ) {
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash(); // To be done after setting values
+    }
+    // Applying SHA 256 helper method to calculate the hash
+    public String calculateHash() {
+        String dataToHash = previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + merkleRoot;
+        return StringUtil.applySha256(dataToHash);
+    }
+
+    // Increases nonce value until hash target is reached.
+    public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDifficultyString(difficulty); // Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((!transaction.processTransaction())) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
+    }
+}
+/* The mineBlock() method takes in an int called difficulty; this is the number of 0’s
+they must solve for. Low difficulty like 1 or 2 can be solved nearly instantly on most computers. */
---
+++ Transaction.java
@@ -0,0 +1,102 @@
+import java.security.*;
+import java.util.ArrayList;
+
+public class Transaction {
+
+    public String transactionId;
+    public PublicKey sender; // address of the sender.
+    public PublicKey recipient; // address of the receiver.
+    public float value; // amount of coins to be transferred.
+    public byte[] signature;
+
+    public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+
+    private static int sequence = 0; // count of how many transactions have been created.
+
+    // Constructor:
+    public Transaction(PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.recipient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+
+    // Helper method that calculates a hash of the transaction.
+    private String calculateHash() {
+        sequence ++; // increase the sequence to avoid two identical transactions having the same hash
+        return StringUtil.applySha256(
+                StringUtil.getStringFromKey(sender) +
+                        StringUtil.getStringFromKey(recipient) +
+                        Float.toString(value) + sequence
+        );
+    }
+
+    // Signs all the data we dont wish to be tampered with.
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value)	;
+        signature = StringUtil.applyECDSASig(privateKey,data);
+    }
+    // Verifies the data we signed hasnt been tampered with
+    public boolean verifySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value)	;
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+    // Returns true if new transaction could be created
+    public boolean processTransaction() {
+
+        if(!verifySignature()) {
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //gather transaction inputs (Make sure they are unspent):
+        for(TransactionInput i : inputs) {
+            i.UTXO = FirstChain.UTXOs.get(i.transactionOutputId);
+        }
+
+        //check if transaction is valid:
+        if(getInputsValue() < FirstChain.minimumTransaction) {
+            System.out.println("#Transaction Inputs to small: " + getInputsValue());
+            return false;
+        }
+
+        //generate transaction outputs:
+        float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+        transactionId = calculateHash();
+        outputs.add(new TransactionOutput( this.recipient, value,transactionId)); //send value to recipient
+        outputs.add(new TransactionOutput( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender
+
+        //add outputs to Unspent list
+        for(TransactionOutput o : outputs) {
+            FirstChain.UTXOs.put(o.id , o);
+        }
+
+        //remove transaction inputs from UTXO lists as spent:
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            FirstChain.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    //returns sum of inputs(UTXOs) values
+    public float getInputsValue() {
+        float total = 0;
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    //returns sum of outputs:
+    public float getOutputsValue() {
+        float total = 0;
+        for(TransactionOutput o : outputs) {
+            total += o.value;
+        }
+        return total;
+    }
+}
